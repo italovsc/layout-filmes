be559d365c6b995a5f0ca1e8d971549f79d5572d
italovsc <81657847+italovsc@users.noreply.github.com>
2021-06-18 21:11:51 -0300

    Update script.js

--- layout-filmes/script.js
+++ layout-filmes/script.js
@@ -1,14 +1,79 @@
-const API_KEY = 'api_key=9edaf34d311ec76e4eadeaa587fb47c9';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9edaf34d311ec76e4eadeaa587fb47c9&language=pt-BR";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=9edaf34d311ec76e4eadeaa587fb47c9&query=";
 
+const LASTED =    
+    "https://api.themoviedb.org/3/discover/movie?sort_by=latest.desc&api_key=9edaf34d311ec76e4eadeaa587fb47c9&language=pt-BR"; 
 
-getMovie(API_URL)
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
+// iniciando com os favoritos
+getMovies(APIURL);
 
-function getMovie(url){
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
 
-    fetch(url).then(res => res.json()).then(data => {
-        console.log(data)
-    })
+    console.log(respData);
+
+    showMovies(respData.results);
 }
+
+function showMovies(movies) {
+    // limpar
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+                <a href="https://www.themoviedb.org/search?language=pt-BR&query=${title}"><img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            
+            
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Sinopse:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
